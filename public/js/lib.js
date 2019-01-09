4bf10507ac7bfb350f82bc36380a18eed738aab6
Smirennikov <evgenii.smirennikov@gmail.com>
2019-01-09 22:35:31 +0300

    add slider partfolio script

---
+++ public/js/lib.js
@@ -0,0 +1,24 @@
+"use strict";
+
+window.onscroll = function () {
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop,
+      screenHeight = window.screen.height,
+      downScreen = scrolled + screenHeight,
+      partfolioArticle = document.querySelectorAll('.section--partfolio__article'),
+      partfolioLength = partfolioArticle.length - 1;
+
+  for (var i = 0; i < partfolioLength; i++) {
+    scrolledPartfolio(i);
+  }
+
+  function scrolledPartfolio(i) {
+    var doubleI = i;
+    doubleI++;
+
+    if (downScreen >= partfolioArticle[i].offsetTop + partfolioArticle[i].offsetHeight) {
+      partfolioArticle[doubleI].style.marginTop = -screenHeight + 88 + 'px';
+    } else {
+      partfolioArticle[doubleI].style.marginTop = 0 + 'px';
+    }
+  }
+};
