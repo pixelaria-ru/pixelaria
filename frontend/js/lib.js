5423f296d98af8ce3962a9745be18f53f9f4f318
Smirennikov <evgenii.smirennikov@gmail.com>
2019-01-11 19:22:46 +0300

    add adaptive 320

--- frontend/js/lib.js
+++ frontend/js/lib.js
@@ -4,11 +4,20 @@ window.onscroll = function() {
   screenHeight = window.screen.height,
   downScreen = scrolled + screenHeight,
   partfolioArticle = document.querySelectorAll('.section--partfolio__article'),
-  partfolioLength = partfolioArticle.length - 1;
+  partfolioLength = partfolioArticle.length - 1,
+  section = document.querySelectorAll('.section'),
+  sectionLength = section.length - 1;
 
+  if(scrolled > document.querySelector('.section--initial-screen').offsetHeight){
+    document.querySelector('.header').classList.add('fixed');
+  }else{
+    document.querySelector('.header').classList.remove('fixed')
+  }
 
-  for(var i = 0; i < partfolioLength; i++){
-    scrolledPartfolio(i);
+  if(window.screen.width > 1000){
+    for(var i = 0; i < partfolioLength; i++){
+      scrolledPartfolio(i);
+    }
   }
 
   function scrolledPartfolio(i){
@@ -20,5 +29,104 @@ window.onscroll = function() {
         partfolioArticle[doubleI].style.marginTop = 0 + 'px';
     }
   }
-  
+
+
+}
+
+if(window.screen.width <= 720){
+  (function(){
+    var mobileSide = document.createElement('div');
+    var mobileMenu = document.createElement('div');
+    var mobileMenuSpan = document.createElement('span');
+    var mobileMenuSvg = document.createElement('svg')
+
+    mobileSide.className = 'header__mobile-side flex';
+    mobileMenu.className = 'mobile-menu flex';
+
+    document.querySelector('.header .container').appendChild(mobileSide);
+    document.querySelector('.header__mobile-side').appendChild(document.querySelector('.header .icon'));
+    document.querySelector('.header__mobile-side').appendChild(mobileMenu);
+    document.querySelector('.mobile-menu').appendChild(mobileMenuSpan);
+
+
+    document.querySelector('.mobile-menu').addEventListener('click', function(){
+      document.querySelector('.header__nav').classList.toggle('active')
+      document.querySelector('.header').classList.toggle('open')
+      document.querySelector('.zak a').classList.add('button','button--green')
+    })
+
+
+  }());
+
+
+  (function heading(){
+    var heading = document.createElement('div');
+    var headingH2 = document.createElement('h2');
+    var headingHr = document.createElement('hr');
+
+    headingH2.innerHTML ='Стоимость услуг'
+    heading.className = 'heading flex';
+    document.querySelector('.section--price .container').prepend(heading);
+    document.querySelector('.section--price .heading').appendChild(headingH2);
+    document.querySelector('.section--price .heading').appendChild(headingHr);
+
+  }());
+  (function(){
+
+    (function heading(){
+      var heading = document.createElement('div');
+      var headingH2 = document.createElement('h2');
+      var headingHr = document.createElement('hr');
+
+      headingH2.innerHTML ='Портфолио'
+      heading.className = 'heading flex';
+      document.querySelector('.section--partfolio').prepend(heading);
+      document.querySelector('.section--partfolio .heading').appendChild(headingH2);
+      document.querySelector('.section--partfolio .heading').appendChild(headingHr);
+
+    }());
+
+    var buttonGreen = document.createElement('button');
+    var partLeftSide = document.querySelectorAll('.partfolio-article__left-side');
+    var partfolioArticle = document.querySelectorAll('.section--partfolio__article');
+    var partfolioArticleLength = partfolioArticle.length;
+
+    buttonGreen.innerHTML ='Смотреть проект';
+    buttonGreen.className = 'button button--green';
+
+    var buttonGreen2 = buttonGreen.cloneNode(true);
+    var buttonGreen3 = buttonGreen.cloneNode(true);
+
+    partfolioArticle[0].querySelectorAll('.partfolio-list__item')[1].append(buttonGreen);
+    partfolioArticle[1].querySelectorAll('.partfolio-list__item')[1].append(buttonGreen2);
+    partfolioArticle[2].querySelectorAll('.partfolio-list__item')[1].append(buttonGreen3);
+
+    for(var i = 0; i < partfolioArticleLength; i++){
+      partLeftSide[i].removeChild(partfolioArticle[i].querySelector('p'))
+      partLeftSide[i].appendChild(partfolioArticle[i].querySelector('h2'))
+      partLeftSide[i].appendChild(partfolioArticle[i].querySelector('span'))
+    }
+
+
+
+
+  }());
+
+
+  (function(){
+
+    (function heading(){
+      var heading = document.createElement('div');
+      var headingH2 = document.createElement('h2');
+      var headingHr = document.createElement('hr');
+
+      headingH2.innerHTML ='О нас'
+      heading.className = 'heading flex';
+      document.querySelector('.section--about').prepend(heading);
+      document.querySelector('.section--about .heading').appendChild(headingH2);
+      document.querySelector('.section--about .heading').appendChild(headingHr);
+
+    }());
+
+  }());
 }
